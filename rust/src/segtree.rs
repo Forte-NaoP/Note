a15103159a696c4ebbd8aae5c7463e26b3c31a47
Forte-NaoP <bae9750@gmail.com>
2024-12-22 22:39:49 +0900

    세그트리 변형 추가

--- rust/src/segtree.rs
+++ rust/src/segtree.rs
@@ -71,3 +71,51 @@ impl SegTree {
         ret
     }
 }
+
+struct SegTreeCmp<T>
+where T: std::cmp::Ord + Copy {
+    t: Vec<T>,
+    base: usize,
+    default: T,
+    cmp: fn(T, T) -> T
+}
+
+impl<T> SegTreeCmp<T>
+where T: std::cmp::Ord + Copy {
+    fn new(mut n: usize, default: T, cmp: fn(T, T) -> T) -> Self {
+        let mut b = 1;
+        while n > 0 {
+            b <<= 1;
+            n >>= 1;
+        }
+        SegTreeCmp { t: vec![default; b << 1], base: b, default, cmp }
+    }
+
+    fn update_bu(&mut self, mut idx: usize, val: T) {
+        idx += self.base;
+        self.t[idx] = val;
+        idx >>= 1;
+        while idx > 0 {
+            self.t[idx] = (self.cmp)(self.t[idx << 1], self.t[idx << 1 | 1]);
+            idx >>= 1;
+        }
+    }
+
+    fn query_td(&self, l: usize, r: usize) -> T {
+        self._query_td(l, r, 0, self.base - 1, 1)
+    }
+
+    fn _query_td(&self, l: usize, r: usize, s: usize, e: usize, idx: usize) -> T {
+        if r < s || e < l {
+            return self.default;
+        }
+        if l <= s && e <= r {
+            return self.t[idx];
+        }
+        let mid = (s + e) / 2;
+        (self.cmp)(
+            self._query_td(l, r, s, mid, idx << 1), 
+            self._query_td(l, r, mid + 1, e, idx << 1 | 1)
+        )
+    }
+}
