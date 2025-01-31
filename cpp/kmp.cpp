1da3d742d67ba950fcb29f810276fdf4981d7b97
Forte-NaoP <bae9750@gmail.com>
2025-01-31 14:46:21 +0900

    KMP 알고리즘 수정

--- cpp/kmp.cpp
+++ cpp/kmp.cpp
@@ -3,42 +3,43 @@
 
 using namespace std;
 
-vector<int> get_pi(const string& s) {
-    vector<int> pi(s.size(), 0);
-    int begin = 1, matched = 0;
-
-    while (begin + matched < s.size()) {
-        if (s[begin+matched] == s[matched]) {
+vector<size_t> get_pi(const string& pattern) {
+    size_t begin = 1, matched = 0, pattern_len = pattern.length();
+    vector<size_t> pi(pattern_len, 0);
+    // pi[i]: pattern[..i]에서 Prefix == Suffix의 최대 길이
+    while (begin + matched < pattern_len) {
+        if (pattern[begin + matched] == pattern[matched]) {
             matched += 1;
-            pi[begin+matched-1] = matched;
+            pi[begin + matched - 1] = matched;
         } else {
             if (matched == 0) begin += 1;
             else {
-                begin += matched - pi[matched-1];
-                matched = pi[matched-1];
+                begin += matched - pi[matched - 1];
+                matched = pi[matched - 1];
             }
         }
     }
     return pi;
 }
 
-int kmp(const string& b, const string& s) {
-    vector<int> pi = get_pi(s);
-    vector<int> found;
-    int begin = 0, matched = 0;
+vector<size_t> kmp(const string& txt, const string& pattern) {
+    size_t txt_len = txt.length(), pattern_len = pattern.length();
+    vector<size_t> pi = get_pi(pattern);
+    vector<size_t> found;
+    size_t begin = 0, matched = 0;
 
-    while (begin + s.size() < b.size()) {
-        if (matched < s.size() && b[begin+matched] == s[matched]) {
+    while (begin <= txt_len - pattern_len) {
+        if (matched < pattern_len && txt[begin + matched] == pattern[matched]) {
             matched += 1;
-            if (matched == s.size()) found.push_back(begin);
+            if (matched == pattern_len) found.push_back(begin);
         } else {
             if (matched == 0) begin += 1;
             else {
-                begin += matched - pi[matched-1];
-                matched = pi[matched-1];
+                begin += matched - pi[matched - 1];
+                matched = pi[matched - 1];
             }
         }
     }
 
-    return found.size();
+    return found;
 }
