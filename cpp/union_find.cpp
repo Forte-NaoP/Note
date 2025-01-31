edce57ea8ae75106ae4286d3f32c88371cbd6da5
Forte-NaoP <bae9750@gmail.com>
2025-01-31 17:07:58 +0900

    main 함수 삭제

--- cpp/union_find.cpp
+++ cpp/union_find.cpp
@@ -22,26 +22,3 @@ void joint(int x, int y) {
     parent[x] = y;
     if (u_rank[x] == u_rank[y]) u_rank[y] = u_rank[x] + 1; 
 }
-
-int main() {
-
-    int T;
-    scanf("%d", &T);
-    int n, m;
-    int a, b;
-    for (int i=1; i<=T; ++i) {
-        scanf("%d%d", &n, &m);
-        for (int j=1; j<=n; ++j) {
-            parent[j] = j;
-            u_rank[j] = 0;
-        }
-        for (int j=0; j<m; ++j) {
-            scanf("%d%d", &a, &b);
-            joint(a, b);
-        }
-
-        for (int j=1; j<=n; ++j) find(j);
-        set<int> s(parent+1, parent+n+1);
-        printf("#%d %lu\n", i, s.size());
-    }
-}
