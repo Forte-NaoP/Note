af8a1969723a27d418579ad69fa0fed6eb23554b
Forte-NaoP <bae9750@gmail.com>
2025-04-19 21:16:06 +0900

    KMP 출처 추가

--- rust/src/kmp.rs
+++ rust/src/kmp.rs
@@ -5,6 +5,7 @@ fn get_pi(s: &[u8]) -> Vec<usize> {
     let mut pi = vec![0; s_len];
     let mut matched = 0;
 
+    // https://yiyj1030.tistory.com/495
     for begin in 1..s_len {
         while matched > 0 && s[begin] != s[matched] {
             matched = pi[matched - 1];
